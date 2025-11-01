efc879264445b1536d52c3b68ef1fc60ac5613eb
rushikeshmj <rushikeshmjadhav021@gmail.com>
2025-11-01 20:11:31 +0530

    Updated bucket name in backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "skalankar-terraform-state"
+    bucket         = "terraform-state-bucket-ecom"
     key            = "eks/${var.environment}/terraform.tfstate"
     region         = "ap-south-1"
     dynamodb_table = "terraform-locks"
